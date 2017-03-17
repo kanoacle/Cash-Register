4bdf5b7725352c2da7eba4c01b6c3cd02aee3f36
Kanojah <kanoacleveland@Kanojahs-MacBook-Pro.local>
2017-03-16 18:26:15 -1000

    cash register file updated, started working on functions

---
+++ js/cash_register.js
@@ -0,0 +1,81 @@
+var myCalc = calculatorModule();
+var memory = myCalc.recallMemory();
+var num = '0.00';
+
+var base = (function () {
+  return parseInt(num);
+})();
+
+function displayNum () {
+  display.innerHTML = '$' + base.toFixed(2);
+}
+
+var cashRegister = (function () {
+
+
+  document.getElementById('deposit').addEventListener('click', function () {
+    memory += display.innerHTML;
+    display.innerHTML = '$' + num.toFixed(2);
+  });
+
+  document.getElementById('balance').addEventListener('click', function () {
+    if (memory === 0) {
+      display.innerHTML = '$' + num.toFixed(2);
+    } else {
+      display.innerHTML = '$' + memory;
+    }
+  });
+
+  document.getElementById('clear').addEventListener('click', function () {
+    display.innerHTML = '$' + num.toFixed(2);
+  });
+
+  document.getElementById('1').addEventListener('click', function () {
+
+  });
+
+  document.getElementById('2').addEventListener('click', function () {
+
+  });
+
+  document.getElementById('3').addEventListener('click', function () {
+
+  });
+
+  document.getElementById('4').addEventListener('click', function () {
+
+  });
+
+  document.getElementById('5').addEventListener('click', function () {
+
+  });
+
+  document.getElementById('6').addEventListener('click', function () {
+
+  });
+
+  document.getElementById('7').addEventListener('click', function () {
+
+  });
+
+  document.getElementById('8').addEventListener('click', function () {
+
+  });
+
+  document.getElementById('9').addEventListener('click', function () {
+
+  });
+
+  document.getElementById('0').addEventListener('click', function () {
+
+  });
+
+  document.getElementById('00').addEventListener('click', function () {
+
+  });
+
+  document.getElementById('dec').addEventListener('click', function () {
+    display.innerHTML += '.';
+  });
+
+})();
